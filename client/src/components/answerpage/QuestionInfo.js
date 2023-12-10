4f0566208908e00893d3f4ae7fd94bf8e069843a
Jeffwu712 <124755490+Jeffwu712@users.noreply.github.com>
2023-12-10 14:41:47 -0500

    change answer asked by

--- client/src/components/answerpage/QuestionInfo.js
+++ client/src/components/answerpage/QuestionInfo.js
@@ -1,9 +1,26 @@
 import React, { Component } from 'react'
 import FormatDate from '../Homepage/FormatDate'
-
+import axios from 'axios'
 
 
 export default class QuestionInfo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      askedByUsername: null,
+    };
+  }
+  async componentDidMount() {
+    const { question } = this.props;
+    if (question.askedBy) {
+      try {
+        const response = await axios.get(`http://localhost:8000/users/${question.askedBy}`);
+        this.setState({ askedByUsername: response.data.username });
+      } catch (error) {
+        console.error('Error fetching username:', error);
+      }
+    }
+  }
     
   hyperLinks(text) {
     const regex = /\[([^\]]*)\]\((https?:\/\/[^\s)]+)\)/g;
@@ -12,12 +29,14 @@ export default class QuestionInfo extends Component {
   }
   
   render() {
+    const { askedByUsername } = this.state;
     return (
+       
       <div className = "anspage_question_data">
           <span className = "bold">{this.props.question.views} views</span>
           <div className="anspage_q_text" dangerouslySetInnerHTML={this.hyperLinks(this.props.question.text)} />
           <div className = "answer_metadata">
-            <div className='mandatory'>{this.props.question.askedBy}</div>
+            <div className='mandatory'>{askedByUsername}</div>
             <div>asked {FormatDate(this.props.question.askDate)}</div>
           </div>
         </div>
