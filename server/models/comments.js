c9f2dfb3f606917f2e71933d8ca9e0f55de8c229
lopez012 <122379590+lopez012@users.noreply.github.com>
2023-12-11 16:16:58 -0500

    comments

--- server/models/comments.js
+++ server/models/comments.js
@@ -6,8 +6,7 @@ var CommentSchema = new Schema({
     text: {type: String, required: true},
     commented_by:{type: Schema.Types.ObjectId, ref: 'User', required: true},
     comment_time:{type:Date , default:Date.now},
-    upvotes: { type: [{ type: Schema.Types.ObjectId, ref: 'User' }], default: [] },  
-    downvotes: { type: [{ type: Schema.Types.ObjectId, ref: 'User' }], default: [] },
+    upvotes: { type: Number, default: 0 },  
 });
 
 
